f8cf486b6e8fbc88bb0f3a98bfa141bb134a9efd
cincu4221 <75069917+cincu4221@users.noreply.github.com>
2021-12-13 15:29:26 +0900

    make it better

--- app.py
+++ app.py
@@ -3,10 +3,20 @@ import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.express as px
+import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
 server = app.server
 
@@ -20,6 +30,19 @@ app.layout = html.Div(
             ],
             className='header'
         ),
+        html.Div(
+            children=[
+                html.H1(children='Hello Dash'),
+                html.Div(children='''
+                    Dash: A web application framework for your data.
+                '''),
+
+                dcc.Graph(
+                    id='example-graph',
+                    figure=fig
+                )
+            ]
+        )
     ]
 )
 
